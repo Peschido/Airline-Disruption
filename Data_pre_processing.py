d640432874c6ef2602c2e08da9207b6e992d8126
stijnbrunia <48210655+stijnbrunia@users.noreply.github.com>
2021-03-15 14:54:53 +0100

    Pre-Processing update

--- Data_pre_processing.py
+++ Data_pre_processing.py
@@ -58,3 +58,36 @@ flights = np.array([["                                      " \
 for i in range(len(flights_raw)): 
     for j in range(len(flights_raw[i][0].split(" "))-1):
         flights[i][j] = flights_raw[i][0].split(" ")[j]
+        
+#####   ITINERARIES FILE
+wb = load_workbook(dataset+"itineraries.xlsx", data_only=True) 
+ws = wb.active 
+
+itineraries_raw = np.array([[i.value for i in j] for j in ws ['A1':'A11213']]) 
+itineraries = np.array([["                                      " \
+           for i in range(len(itineraries_raw[1][0].split(" ")) -1)] for j in range(len(itineraries_raw))])
+for i in range(len(itineraries_raw)): 
+    for j in range(len(itineraries_raw[i][0].split(" "))-1):
+        itineraries[i][j] = itineraries_raw[i][0].split(" ")[j]
+        
+#####   POSITIONS FILE
+wb = load_workbook(dataset+"position.xlsx", data_only=True) 
+ws = wb.active 
+
+position_raw = np.array([[i.value for i in j] for j in ws ['A1':'A44']]) 
+position = np.array([["                                      " \
+           for i in range(len(position_raw[8][0].split(" ")) -1)] for j in range(len(position_raw))])
+for i in range(len(position_raw)): 
+    for j in range(len(position_raw[i][0].split(" "))-1):
+        position[i][j] = position_raw[i][0].split(" ")[j]
+        
+#####   ROTATIONS FILE
+wb = load_workbook(dataset+"rotations.xlsx", data_only=True) 
+ws = wb.active 
+
+rotations_raw = np.array([[i.value for i in j] for j in ws ['A1':'A2844']]) 
+rotations = np.array([["                                      " \
+           for i in range(len(rotations_raw[8][0].split(" ")) -1)] for j in range(len(rotations_raw))])
+for i in range(len(rotations_raw)): 
+    for j in range(len(rotations_raw[i][0].split(" "))-1):
+        rotations[i][j] = rotations_raw[i][0].split(" ")[j]
